5461d45e989772d425e1807e687da4d8c628a0de
matti909 <matias.saantiago@gmail.com>
2023-08-17 12:25:01 -0300

    render29

--- api2/src/index.ts
+++ api2/src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { app } from './app';
-import { AppDS } from '../src/config/ormconfig';
+import { AppDS } from './../src/config/ormconfig';
 
 require('dotenv').config();
 const PORT = process.env.PORT;
