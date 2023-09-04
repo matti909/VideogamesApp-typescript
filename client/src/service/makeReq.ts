74544cb6068a9face112fdc3b1a64731ce5907ff
matti909 <matias.saantiago@gmail.com>
2023-09-04 11:40:12 -0300

    .

--- client/src/service/makeReq.ts
+++ client/src/service/makeReq.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//export const BASEURL = "https://games-e8uu.onrender.com";
-export const BASEURL = "http://localhost:4002";
+export const BASEURL = "https://games-e8uu.onrender.com";
+//export const BASEURL = "http://localhost:4002";
 
 export const instance = axios.create({
   baseURL: BASEURL,
